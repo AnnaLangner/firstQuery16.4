3b2656fc251e143da2929e905079689beee24d2f
Ania <annalangner06@gmail.com>
2019-05-01 11:23:30 +0200

    Adding ubdate to js/script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@
 
 var url = 'http://api.icndb.com/jokes/random';
 
-var btnJoke = dokument.getElementById('get-joke');
-btnJoke.addEventListener('click' function () {
+getJoke();
+
+var btnJoke = document.getElementById('get-joke');
+btnJoke.addEventListener('click', function () {
 	getJoke();
 });
 
-var paragraph = dokument.getElementById('joke');
+var paragraph = document.getElementById('joke');
 
 function getJoke() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url);
+
 	xhr.addEventListener('load', function() {
 		var response = JSON.parse(xhr.response);
 		paragraph.innerHTML = response.value.joke;
